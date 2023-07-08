ec864e4b3bd898701e9ff4ad29aeefbec2b69801
Wesley Kozach Rubel <wkozach@gmail.com>
2023-07-08 10:31:27 -0300

    Create exemplo004.js

---
+++ arrays/exemplo004.js
@@ -0,0 +1,4 @@
+var idades = [5,6,8,3,6,9]
+console.log(idades.indexOf(6)) //Retorna o índice 1 (Posição onde o elemento esta guardado)
+console.log(idades.lastIndexOf(6)) //Retorna o índice 4
+console.log(idades.indexOf(7))
