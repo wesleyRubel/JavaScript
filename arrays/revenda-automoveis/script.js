720ec755e470bf573a9cdd36c4d89973807dd697
Wesley Kozach Rubel <wkozach@gmail.com>
2023-07-11 18:52:38 -0300

    Finalizei Exercício

--- arrays/revenda-automoveis/script.js
+++ arrays/revenda-automoveis/script.js
@@ -29,9 +29,41 @@ function adicionar() {
     var p = Number(pre.value)
     carros.push({ modelo: m, preco: p })
     for (var i = 0; i < carros.length; i++) {
-        res.innerHTML += `${carros[i].modelo} - R$: ${carros[i].preco} <br>`
+        res.innerHTML += `${carros[i].modelo} - R$: ${carros[i].preco} <br><br>`
     }
     mod.value = ''
     mod.focus()
     pre.value = ''
+}
+
+function listar(){
+    if(carros.length == 0){
+        window.alert('Não existem veículos para listar')
+        window.document.getElementById('mod').focus()
+        return
+    }
+    res.innerHTML = ''
+    for(var i = 0;i < carros.length;i++){
+        res.innerHTML += `${carros[i].modelo} - R$: ${carros[i].preco} <br><br>`
+    }
+}
+
+function filtrar(){
+    if(carros.length == 0){
+        window.alert('Não exitem veículos para filtrar')
+        window.document.getElementById('mod').focus()
+        return
+    }
+    var valor = Number(prompt('Pesquisar valores até R$:'))
+    res.innerHTML = ''
+    var Nenc = false
+    for(var i = 0;i < carros.length;i++){
+        if(carros[i].preco <= valor){
+            res.innerHTML += `${carros[i].modelo} - R$: ${carros[i].preco} <br><br>`
+            Nenc = true
+        }
+    }
+    if(!Nenc){
+        window.alert(`Não foram encontrados veículos com valor até R$: ${valor}`)
+    }
 }
