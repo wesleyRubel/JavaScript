f87a8bb1f2f11a30d78c099d003830feba40327f
Wesley Kozach Rubel <wkozach@gmail.com>
2023-07-19 10:04:45 -0300

    Create exemplo006.js

---
+++ arrays/exemplo006.js
@@ -0,0 +1,12 @@
+/*var nomes = ["Pedro","Ana","João","Fabio","Carlos","José","Adrian"]
+console.log(nomes)
+nomes.sort()
+console.log(nomes)
+nomes.reverse()
+console.log(nomes)*/
+var numeros = [100,50,2,10,15]
+console.log(numeros)
+numeros.sort()
+console.log(numeros)
+numeros.sort(function(a,b){return a-b})
+console.log(numeros)
