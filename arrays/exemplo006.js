301642e65448c9401df62213b122de13f55909dc
Wesley Kozach Rubel <wkozach@gmail.com>
2023-07-19 11:29:08 -0300

    Update exemplo006.js

--- arrays/exemplo006.js
+++ arrays/exemplo006.js
@@ -3,10 +3,15 @@ console.log(nomes)
 nomes.sort()
 console.log(nomes)
 nomes.reverse()
-console.log(nomes)*/
+console.log(nomes)
 var numeros = [100,50,2,10,15]
 console.log(numeros)
 numeros.sort()
 console.log(numeros)
 numeros.sort(function(a,b){return a-b})
-console.log(numeros)
+console.log(numeros)*/
+
+var pessoas = [{nome:'Wesley',idade:31},{nome:'Lucas',idade:20},{nome:'Cintia',idade:33}]
+console.log(pessoas)
+pessoas.sort()
+console.log(pessoas)
